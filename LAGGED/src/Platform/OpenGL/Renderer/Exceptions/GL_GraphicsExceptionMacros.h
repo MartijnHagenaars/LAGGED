5442cc501f9eaf114f4b140004925ee17b8dd6b2
MartijnHag <martijnhagenaars@outlook.com>
2024-04-01 19:11:10 +0200

    [Removed] - OpenGL graphics exception class
    It's no longer used, so removing it here.

--- LAGGED/src/Platform/OpenGL/Renderer/Exceptions/GL_GraphicsExceptionMacros.h
+++ LAGGED/src/Platform/OpenGL/Renderer/Exceptions/GL_GraphicsExceptionMacros.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "GL_GraphicsException.h"
 #include "Utility/Logger.h"
 #include "Platform/OpenGL/Renderer/GL_ErrorCodeLookup.h"
 
@@ -7,7 +6,6 @@
 //https://www.khronos.org/opengl/wiki/OpenGL_Error#Catching_errors_(the_easy_way)
 
 #ifdef DEBUG
-//#define LAG_GRAPHICS_EXCEPTION(x) x; { GLenum glError; while((glError = glGetError()) != GL_NO_ERROR) throw GraphicsException(__LINE__, __FILE__, glError ); }
 #define LAG_GRAPHICS_EXCEPTION(x) x; { GLenum glError; while((glError = glGetError()) != GL_NO_ERROR) {LAG::Logger::Critical("OpenGL Error: [{0}][{1}]: OpenGL error: {2}", __FILE__, __LINE__, LAG::ConvertErrorToString(glError)); __debugbreak();} }
 #else
 #define LAG_GRAPHICS_EXCEPTION(x) x;
