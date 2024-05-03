26a4f6c92346bfdc70fa85bc9957e2501d2c54b0
MartijnHag <martijnhagenaars@outlook.com>
2024-05-03 23:46:08 +0200

    [Changed] - Slightly improved Scene documentation

--- LAGGED/src/ECS/Scene.h
+++ LAGGED/src/ECS/Scene.h
@@ -34,6 +34,12 @@ namespace LAG
 
 		void RemoveAll();
 
+		/// <summary>
+		/// This function is used to loop over all entities in the scene.
+		/// </summary>
+		/// <typeparam name="...Components">The component types that an entity needs to contain.</typeparam>
+		/// <param name="func">The function that is called when looping over an entity that contains all required components.</param>
+		/// <returns>Returns true if any entities are found containing all required components..</returns>
 		template<typename... Components>
 		bool Loop(std::function<void(Entity entity, Components&...)> func)
 		{
@@ -50,8 +56,10 @@ namespace LAG
 			return true;
 		}
 
-		//Loops over all components. When looping, the function pointer is called for every registered component type.
-		//When calling the function pointer, an object of type ComponentData is passed in. This object contains info about the component like name and ID. 
+		/// <summary>
+		/// Loops over all components. When looping, the function pointer is called for every registered component type.
+		/// </summary>
+		/// <param name="func">The function that is called when looping over a registered component. When calling the function pointer, an object of type ComponentData is passed in. This object contains info about the component like name and ID.</param>
 		void ComponentLoop(std::function<void(ComponentData& compData)> func);
 
 		void HandleComponentWidgets(Entity* entity, Reflection::WidgetModes mode);
