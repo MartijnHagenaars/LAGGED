bbbef18cb7dbeb85e17789763f97f10ae54fb612
MartijnHag <martijnhagenaars@outlook.com>
2024-05-20 12:24:30 +0200

    [Added] - Documentation for component and variable setup functions

--- LAGGED/src/ECS/Meta/ReflectionComponentSetup.h
+++ LAGGED/src/ECS/Meta/ReflectionComponentSetup.h
@@ -30,7 +30,18 @@ namespace LAG
 			ComponentReflectionSetup& operator=(const ComponentReflectionSetup& other); //Copy assignment operator
 			~ComponentReflectionSetup();
 
+			/// <summary>
+			/// Set the display name of the component in the editor.
+			/// </summary>
+			/// <param name="displayName">Name that is displayed in the editor.</param>
+			/// <returns>The current instance for chaining further configuration.</returns>
 			ComponentReflectionSetup& SetDisplayName(const std::string& displayName);
+
+			/// <summary>
+			/// Set whether the component is visible in the editor.
+			/// </summary>
+			/// <param name="isVisible">When set to true, the component is visible in the editor.</param>
+			/// <returns>The current instance for chaining further configuration.</returns>
 			ComponentReflectionSetup& SetVisibleInEditor(bool isVisible);
 
 		private:
@@ -56,8 +67,21 @@ namespace LAG
 			VariableReflectionSetup& operator=(const VariableReflectionSetup& other); //Copy assignment operator
 			~VariableReflectionSetup();
 
+			/// <summary>
+			/// Set the display name of the variable in the editor.
+			/// </summary>
+			/// <param name="displayName">Name that is displayed in the editor.</param>
+			/// <returns>The current instance for chaining further configuration.</returns>
 			VariableReflectionSetup& SetDisplayName(const std::string& displayName);
+
+			//TODO: Needs to be implemented.
 			VariableReflectionSetup& SetCallbackFunction();
+
+			/// <summary>
+			/// Sets the variable's read-only status.
+			/// </summary>
+			/// <param name="isReadOnly">When set to true, the variable cannot be changed in the editor.</param>
+			/// <returns>The current instance for chaining further configuration.</returns>
 			VariableReflectionSetup& SetReadOnly(bool isReadOnly);
 
 		private:
@@ -75,9 +99,20 @@ namespace LAG
 		class ReflectionSystem
 		{
 		public:
+			/// <summary>
+			/// Registers a component for use in the editor.
+			/// </summary>
+			/// <typeparam name="T">The component type</typeparam>
+			/// <returns>The class for configuring the reflection of the component. Uses the builder pattern.</returns>
 			template<typename T>
 			ComponentReflectionSetup<T> RegisterComponent() { return ComponentReflectionSetup<T>(); };
 
+			/// <summary>
+			/// Registers a variable from a specific component. Registering the variable allows it to be used in the editor.
+			/// </summary>
+			/// <typeparam name="ClassType">The component type.</typeparam>
+			/// <typeparam name="Variable">The variable / member function pointer.</typeparam>
+			/// <returns>The class for configuring the reflection of the variable. Uses the builder pattern.</returns>
 			template<typename ClassType, auto Variable>
 			VariableReflectionSetup<ClassType, Variable> RegisterVariable() { return VariableReflectionSetup<ClassType, Variable>(); };
 		};
@@ -109,14 +144,14 @@ namespace LAG
 		template<typename ClassType>
 		inline ComponentReflectionSetup<ClassType>& ComponentReflectionSetup<ClassType>::SetDisplayName(const std::string& displayName)
 		{
-			m_Factory->prop(ComponentProperties::DISPLAY_NAME, displayName); 
+			m_Factory->prop(ComponentProperties::DISPLAY_NAME, displayName);
 			return *this;
 		}
 
 		template<typename ClassType>
 		inline ComponentReflectionSetup<ClassType>& ComponentReflectionSetup<ClassType>::SetVisibleInEditor(bool isVisible)
 		{
-			m_Factory->prop(ComponentProperties::VISIBLE_IN_EDITOR, false); 
+			m_Factory->prop(ComponentProperties::VISIBLE_IN_EDITOR, false);
 			return *this;
 		}
 
@@ -157,16 +192,16 @@ namespace LAG
 		}
 
 		template<typename ClassType, auto Variable>
-		inline VariableReflectionSetup<ClassType, Variable> & VariableReflectionSetup<ClassType, Variable>::SetCallbackFunction()
+		inline VariableReflectionSetup<ClassType, Variable>& VariableReflectionSetup<ClassType, Variable>::SetCallbackFunction()
 		{
 			LAG_ASSERT("TODO: Implement"); return *this;
 		}
 
 		template<typename ClassType, auto Variable>
-		inline VariableReflectionSetup<ClassType, Variable> & VariableReflectionSetup<ClassType, Variable>::SetReadOnly(bool isReadOnly)
+		inline VariableReflectionSetup<ClassType, Variable>& VariableReflectionSetup<ClassType, Variable>::SetReadOnly(bool isReadOnly)
 		{
 			LAG_ASSERT("TODO: Implement"); return *this;
 		}
 
-};
+	};
 }
