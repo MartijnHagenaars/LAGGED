4a2c4d78ab9a2ead9aead6b8cb62296cded0a1e0
MartijnHag <martijnhagenaars@outlook.com>
2024-07-17 17:52:39 +0200

    [Changed] - Another approach for logging variables

--- LAGGED/src/Utility/StringUtility.h
+++ LAGGED/src/Utility/StringUtility.h
@@ -5,47 +5,35 @@ namespace LAG
 {
 	namespace Utility
 	{
-        //Functions for converting variables into strings
+		//Functions for converting variables into strings
 
-        template<typename T>
-        inline std::string ConvertToString(T variable)
-        {
-            return std::string("[UNDEFINED VARIABLE TYPE]");
-        }
+		template<typename T>
+		typename std::enable_if<std::is_arithmetic<T>::value, std::string>::type
+			inline ConvertToString(T variable)
+		{
+			return std::to_string(variable);
+		}
 
-        // Specialization for bool
-        template<>
-        inline std::string ConvertToString<bool>(bool variable)
-        {
-            return variable ? "true" : "false";
-        }
+		// Primary template for non-arithmetic types
+		template<typename T>
+		typename std::enable_if<!std::is_arithmetic<T>::value, std::string>::type
+			inline ConvertToString(T variable)
+		{
+			return std::string("[UNDEFINED VARIABLE TYPE]");
+		}
 
-        // Specialization for string
-        template<>
-        inline std::string ConvertToString<std::string>(std::string variable)
-        {
-            return variable;
-        }
+		// Specialization for bool
+		template<>
+		inline std::string ConvertToString<bool>(bool variable)
+		{
+			return variable ? "true" : "false";
+		}
 
-        // Specialization for int
-        template<>
-        inline std::string ConvertToString<int>(int variable)
-        {
-            return std::to_string(variable);
-        }
-
-        // Specialization for unsigned int
-        template<>
-        inline std::string ConvertToString<unsigned int>(unsigned int variable)
-        {
-            return std::to_string(variable);
-        }
-
-        // Specialization for float
-        template<>
-        inline std::string ConvertToString<float>(float variable)
-        {
-            return std::to_string(variable);
-        }
+		// Specialization for std::string
+		template<>
+		inline std::string ConvertToString<std::string>(std::string variable)
+		{
+			return variable;
+		}
 	}
 }
