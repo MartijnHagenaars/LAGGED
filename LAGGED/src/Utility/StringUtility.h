2ef1c703d8546017f0376c04b223b021bd46cd1d
MartijnHag <martijnhagenaars@outlook.com>
2024-07-17 17:54:42 +0200

    [Changed] - Better template overloading

--- LAGGED/src/Utility/StringUtility.h
+++ LAGGED/src/Utility/StringUtility.h
@@ -8,7 +8,9 @@ namespace LAG
 		//Functions for converting variables into strings
 
 		template<typename T>
-		typename std::enable_if<std::is_arithmetic<T>::value, std::string>::type
+		typename std::enable_if<std::is_same<T, int>::value || std::is_same<T, long>::value || std::is_same<T, long long>::value ||
+			std::is_same<T, unsigned>::value || std::is_same<T, unsigned long>::value || std::is_same<T, unsigned long long>::value ||
+			std::is_same<T, float>::value || std::is_same<T, double>::value || std::is_same<T, long double>::value, std::string>::type
 			inline ConvertToString(T variable)
 		{
 			return std::to_string(variable);
@@ -16,7 +18,9 @@ namespace LAG
 
 		// Primary template for non-arithmetic types
 		template<typename T>
-		typename std::enable_if<!std::is_arithmetic<T>::value, std::string>::type
+		typename std::enable_if<!std::is_same<T, int>::value && !std::is_same<T, long>::value && !std::is_same<T, long long>::value &&
+			!std::is_same<T, unsigned>::value && !std::is_same<T, unsigned long>::value && !std::is_same<T, unsigned long long>::value &&
+			!std::is_same<T, float>::value && !std::is_same<T, double>::value && !std::is_same<T, long double>::value, std::string>::type
 			inline ConvertToString(T variable)
 		{
 			return std::string("[UNDEFINED VARIABLE TYPE]");
@@ -27,13 +31,6 @@ namespace LAG
 		inline std::string ConvertToString<bool>(bool variable)
 		{
 			return variable ? "true" : "false";
-		}
-
-		// Specialization for std::string
-		template<>
-		inline std::string ConvertToString<std::string>(std::string variable)
-		{
-			return variable;
 		}
 	}
 }
