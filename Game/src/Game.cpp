30d0874ad23dcd7081ccfb08df7cc10c4fd2d316
MartijnHag <martijnhagenaars@outlook.com>
2025-06-17 23:55:56 +0200

    [Changed] - Different Game Update test

--- Game/src/Game.cpp
+++ Game/src/Game.cpp
@@ -24,6 +24,60 @@
 
 ENTRY_APP(Game)
 
+namespace LAG
+{
+	struct InputComponent
+	{
+		bool jump = false;
+		bool shoot = false;
+
+		float leftStickX = 0.0f;
+		float leftStickY = 0.0f;
+		float rightStickX = 0.0f;
+		float rightStickY = 0.0f;
+	};
+
+	struct PhysicsComponent
+	{
+		float mass = 1.0f;
+		float drag = 0.0f;
+		bool useGravity = true;
+
+		float velocityX = 0.0f;
+		float velocityY = 0.0f;
+		float velocityZ = 0.0f;
+
+		float forceX = 0.0f;
+		float forceY = 0.0f;
+		float forceZ = 0.0f;
+
+
+		float data[5120];
+	};
+
+	struct AudioComponent
+	{
+		bool playing = false;
+		std::string audioPath;
+		bool loop = false;
+		float volume = 1.f;
+		float attenuation = 1.f;
+
+
+		float data[5120];
+	};
+
+	struct EnemyComponent
+	{
+		bool alive;
+		std::string name;
+
+		float health;
+		float anger;
+		float strength;
+	};
+}
+
 Game::Game()
 {
 }
@@ -40,6 +94,7 @@ void Game::Initialize()
 
 	LAG::GetRenderer()->SetSkyboxCubemap(LAG::HashedString("res/Assets/Cubemaps/Sky_2k"));
 	LAG::GetResourceManager()->AddResource<LAG::Model>(LAG::HashedString("res/Assets/Models/Helmet/DamagedHelmet.gltf"));
+	LAG::GetResourceManager()->AddResource<LAG::Model>(LAG::HashedString("res/Assets/Models/Cube/Cube.gltf"));
 	//LAG::GetResourceManager()->AddResource<LAG::Model>(LAG::HashedString("res/Assets/Models/Avocado/Avocado.gltf"));
 	//LAG::GetResourceManager()->AddResource<LAG::Model>(LAG::HashedString("res/Assets/Models/BoomBox/BoomBox.gltf"));
 	//LAG::GetResourceManager()->AddResource<LAG::Model>(LAG::HashedString("res/Assets/Models/ChessGame/ABeautifulGame.gltf"));
@@ -77,7 +132,39 @@ void Game::Initialize()
 	ent5.GetComponent<LAG::TransformComponent>()->SetRotation(glm::vec3(1.57f, 0.f, 0.f));
 	ent5.AddComponent<LAG::CameraComponent>()->movementSpeed = 50.f;
 
-	m_World = new World(12, true);
+	//m_World = new World(12, true);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	const int TOTAL_DIMENSION = 50;
+	srand(3);
+
+	LAG::Scene* scene = LAG::GetScene();
+	for (int y = 0; y < TOTAL_DIMENSION; y++)
+	{
+
+		int enemyIndex = rand() % TOTAL_DIMENSION + 1;
+		int inputIndex = rand() % TOTAL_DIMENSION + 1;
+		const int withAudio = static_cast<int>(TOTAL_DIMENSION * 0.3f);
+		const int withPhysics = static_cast<int>(TOTAL_DIMENSION * 0.5f);
+
+		for (int x = 0; x < TOTAL_DIMENSION; x++)
+		{
+			LAG::Entity entity = scene->AddEntity();
+			entity.AddComponent<LAG::TransformComponent>();
+			//entity.AddComponent<LAG::ModelComponent>("res/Assets/Models/Cube/Cube.gltf");
+
+			if (x > withAudio)
+				entity.AddComponent<LAG::AudioComponent>();
+			if (x > withPhysics)
+				entity.AddComponent<LAG::PhysicsComponent>();
+
+			if (enemyIndex == x)
+				entity.AddComponent<LAG::EnemyComponent>();
+			if (inputIndex == x)
+				entity.AddComponent<LAG::InputComponent>();
+		}
+	}
 }
 
 void Game::Shutdown()
@@ -97,4 +184,22 @@ void Game::Update()
 			transform->SetPosition(glm::vec3(currPos.x, currPos.y + 0.01f, currPos.z));
 		}
 	);
+
+	scene->RunSystem<LAG::TransformComponent, LAG::ModelComponent, LAG::PhysicsComponent, LAG::EnemyComponent>(
+		[](
+			LAG::EntityID entity,
+			LAG::TransformComponent* transform, LAG::ModelComponent* model,
+			LAG::PhysicsComponent* physics, LAG::EnemyComponent* enemy)
+		{
+			enemy->strength += 0.1f;
+		});
+
+	scene->RunSystem<LAG::TransformComponent, LAG::ModelComponent, LAG::AudioComponent, LAG::InputComponent>(
+		[](
+			LAG::EntityID entity,
+			LAG::TransformComponent* transform, LAG::ModelComponent* model,
+			LAG::AudioComponent* audio, LAG::InputComponent* input)
+		{
+			input->leftStickX += 0.1f;
+		});
 }
