af8c7fe71abbb8add9be2e36f750597a0032d1c5
MartijnHag <martijnhagenaars@outlook.com>
2025-07-15 19:30:07 +0200

    [Changed] - ProfilerViewer code cleanup

--- LAGGED/src/Editor/Tools/ProfilerViewer.cpp
+++ LAGGED/src/Editor/Tools/ProfilerViewer.cpp
@@ -14,32 +14,32 @@ namespace LAG
 	{
 		Profiler::CalculateAverages();
 
-		if (ImGui::BeginTable("records", 3, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg | ImGuiTableFlags_Resizable))
+		float accumAvg = 0.f;
+		ImGui::BeginTable("records", 3, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg | ImGuiTableFlags_Resizable);
+		
+		// Setup table
+		ImGui::TableSetupColumn("Name");
+		ImGui::TableSetupColumn("Avg. time");
+		ImGui::TableSetupColumn("Samples");
+		ImGui::TableHeadersRow();
+
+		// Populate table with data
+		const auto& records = Profiler::GetRecords();
+		for (auto& it : records)
 		{
-			// Display headers so we can inspect their interaction with borders.
-			// (Headers are not the main purpose of this section of the demo, so we are not elaborating on them too much. See other sections for details)
+			const Profiler::ProfilerRecord& rec = it.second;
+			ImGui::TableNextRow();
+			ImGui::TableSetColumnIndex(0);
+			ImGui::Text("%s", it.first.c_str());
+			ImGui::TableSetColumnIndex(1);
+			ImGui::Text("%fms", rec.average);
+			ImGui::TableSetColumnIndex(2);
+			ImGui::Text("%i", rec.history.size());
 
-			ImGui::TableSetupColumn("Name");
-			ImGui::TableSetupColumn("Avg. time");
-			ImGui::TableSetupColumn("Samples");
-			ImGui::TableHeadersRow();
-
-			float accumAvg = 0.f;
-			const auto& records = Profiler::GetRecords();
-			for (auto& it : records)
-			{
-				const Profiler::ProfilerRecord & rec = it.second;
-				ImGui::TableNextRow();
-				ImGui::TableSetColumnIndex(0);
-				ImGui::Text("%s", it.first.c_str());
-				ImGui::TableSetColumnIndex(1);
-				ImGui::Text("%fms", rec.average);
-				ImGui::TableSetColumnIndex(2);
-				ImGui::Text("%i", rec.history.size());
-
-				accumAvg += rec.average;
-			}
-			ImGui::EndTable();
+			accumAvg += rec.average;
 		}
+		ImGui::EndTable();
+
+		ImGui::Text("Total accumulated average: %f", accumAvg);
 	}
 }
